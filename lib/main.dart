186c62e5034b3904e3e3f2ee333127143e30878b
henmu <henri.mursu@gmail.com>
2022-01-26 19:29:12 +0200

    basic UI design without functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,117 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:fullscreen/fullscreen.dart';
 
 void main() {
-  var applicationBarText = Text('My Application');
-  var topBar = AppBar(title: applicationBarText);
-  var textContents = Text('Hello Oamk Student!');
-  var centerText = Center(child: textContents);
-  var coreUi = Scaffold(appBar: topBar, body: centerText);
-  var myApp = MaterialApp(home: coreUi);
+  runApp(const MyApp());
+}
 
-  runApp(myApp);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'hotorcold',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Hotter or Colder Game'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style =
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+
+    return Scaffold(
+      backgroundColor: Colors.grey.shade800,
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Container(
+        alignment: Alignment.topCenter,
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const Text(
+              'Is "City Name" hotter or colder than "Second City Name"',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Roboto',
+                letterSpacing: 0.5,
+                fontSize: 40,
+              ),
+            ),
+            const SizedBox(height: 80),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                fixedSize: const Size(300, 70),
+                primary: Colors.red,
+                onPrimary: Colors.black87,
+                textStyle: const TextStyle(fontSize: 50),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+              ),
+              onPressed: () {},
+              child: const Text('Hotter'),
+            ),
+            const SizedBox(height: 27),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue.shade600,
+                onPrimary: Colors.black,
+                textStyle: const TextStyle(fontSize: 50),
+              ),
+              onPressed: () {},
+              child: const Text('Colder'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
